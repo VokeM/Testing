0c5128928f05be02d8c5e3005b7fc8080f7c6782
VokeM <56253990+VokeM@users.noreply.github.com>
2021-01-22 14:19:48 +0100

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -22,29 +22,7 @@ import pyodbc
 
 warnings.filterwarnings('ignore')
 
-#importing the datasets
-ds = pd.read_csv("labeledTrainData.tsv", delimiter="\t")
-train = ds.drop(['id'], axis=1)
-
-X1 = train['review'].values
-y = train['sentiment'].values
-
-token = RegexpTokenizer(r'[a-zA-Z0-9]+')
-cv = CountVectorizer(stop_words = 'english', ngram_range = (1,2), tokenizer = token.tokenize).fit(X1)
-X = cv.transform(X1)
-
-#splitting gthe dataset
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle= True, random_state=42)
-
-# training our classifier ; train_data.target will be having numbers assigned for each category in train data
-clf = MultinomialNB().fit(X_train, y_train)
-
-#pickle.dump(clf,open('NBmodel.pkl','wb'))
-#pickle.dump(cv,open('vector.pkl','wb'))
-
-predicted = clf.predict(X_test)
-accuracy_score = metrics.accuracy_score(predicted, y_test)
-print(str('{:04.2f}'.format(accuracy_score * 100))+'%')
+print('I am a girl')
 
 ServerName='SQL5053.site4now.net'
 MSQLDatabase='DB_A5DF1B_jobcenta'
