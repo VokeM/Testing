f0d15155f157b598f8c4638affb51c745fbd0ddb
VokeM <56253990+VokeM@users.noreply.github.com>
2021-01-22 14:21:30 +0100

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -24,17 +24,12 @@ warnings.filterwarnings('ignore')
 
 print('I am a girl')
 
-ServerName='SQL5053.site4now.net'
-MSQLDatabase='DB_A5DF1B_jobcenta'
-username='DB_A5DF1B_jobcenta_admin'
-password='admin2019'
-#import mysql.connector as connection
-import adodbapi
-conn = adodbapi.connect("PROVIDER=SQLOLEDB;Data Source={0};Database={1}; \
-       trusted_connection=yes;UID={2};PWD={3};".format(ServerName,MSQLDatabase,username,password))
-cursor = conn.cursor()
+cnxn = pyodbc.connect("Driver={SQL Server};"
+                        "Server=SQL5053.site4now.net;"
+                        "Database=DB_A5DF1B_jobcenta;"
+                        "uid=DB_A5DF1B_jobcenta_admin;pwd=admin2019")
+ds = pd.read_sql_query('select * from ArtisanReviews', cnxn)
 
-ds = pd.read_sql_query('select * from ArtisanReviews', cursor)
 app = Flask(__name__)
 CORS(app)
 @app.route('/sentiment/', methods=['GET'])
@@ -43,12 +38,8 @@ def make_prediction():
     if flask.request.method == 'GET':
         ID='03bf12c6-72ee-4fab-8982-585242da2064'
         Art=user(ID)
-        X2 = Art['Comment'].values
-        X_new = cv.transform(X2)# building up feature vector of our input
-        prediction = clf.predict(X_new) #making predictions
-        zeros = int(np.sum(prediction == 0))#counting the number of zeros in the analysis
-        ones = int(np.sum(prediction == 1))#counting the number of ones in the analysis
-        return flask.jsonify(zeros,ones)#returning the zeros and ones
+        X2 = Art['Comment'].values        
+        return flask.jsonify(X2)#returning the zeros and ones
 
 def user(userid):
     df=ds.sort_values('ArtisanID')
@@ -57,6 +48,4 @@ def user(userid):
     return Art
 
 if __name__ == '__main__':
-    clf = joblib.load("NBmodel.pkl")#loading the already trained model
-    cv = joblib.load("vector.pkl")#loading the count vectorizer that will be used to vectorize the new inputs
     app.run(debug=True, use_reloader=False,threaded=False)
